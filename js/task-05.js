661eeb1266bb32bd36a3d7357fea8d448e54d0bf
Kotlyk <irashakot@gmail.com>
2022-09-17 11:32:24 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#name-input");
+let nameEl = document.querySelector("#name-output");
+
+input.addEventListener('input', onInput);
+function onInput(evt) {
+    nameEl.textContent = evt.currentTarget.value.trim();
+};
