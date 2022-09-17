44376cbfa9849016fa503d30d20d94d66dd99fa8
Kotlyk <irashakot@gmail.com>
2022-09-17 20:52:13 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const colorBtn = document.querySelector("button");
+console.dir(colorBtn);
+
+
+colorBtn.addEventListener("click", onClick);
+function onClick(getRandomHexColor) {
+  document.body.style.background =
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+};
+  
+
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// };
