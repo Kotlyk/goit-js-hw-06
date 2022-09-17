873bee6eb8bedb353471708244bb9a86ff0a0a8c
Kotlyk <irashakot@gmail.com>
2022-09-17 13:15:54 +0300

    create task 8

---
+++ js/task-06.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#validation-input");
+const color = document,querySelector('')
+
+input.addEventListener("input", onInput);
+function onInput(evt) {
+    if (evt.currentTarget.value.trim().length = 6) {
+       
+   } 
+}
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = {
+    mail,
+    password,
+  };
+     if (
+       formElements.email.value === "" ||
+       formElements.password.value === ""
+     ) {
+       return alert("Please fill in all the fields!");
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+}
