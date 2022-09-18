a44b71c1b52b4a8c3de58f50e813fe46eac5f1a7
Kotlyk <irashakot@gmail.com>
2022-09-18 17:16:11 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,15 +5,21 @@ console.log('Number of categories:', categEl.length);
 console.log(categEl);
 
 
+categEl.forEach(function (item) {
+  const titleEl = item.firstElementChild.textContent;
+  const amountEl = item.lastElementChild.childElementCount;
+  console.log(`Category:`, titleEl);
+  console.log(`Elements:`, amountEl);
+});
 
 
-for (let i = 0; i < categEl.length; i+=1) {
-    const titleEl = categEl[i].firstElementChild.textContent;
-    const amountEl = categEl[i].lastElementChild.childElementCount;
-    console.log(`Category:`, titleEl);
-    console.log(`Elements:`, amountEl);
+// for (let i = 0; i < categEl.length; i+=1) {
+//     const titleEl = categEl[i].firstElementChild.textContent;
+//     const amountEl = categEl[i].lastElementChild.childElementCount;
+//     console.log(`Category:`, titleEl);
+//     console.log(`Elements:`, amountEl);
     
-}
+// }
 
 
 
